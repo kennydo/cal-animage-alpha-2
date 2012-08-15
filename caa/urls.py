a80397b8e5d5f1e262aa907f68ec940c52f840c3
Kenny Do <kennydo@berkeley.edu>
2012-08-15 15:27:02 -0700

    added urls of the current website

--- caa/urls.py
+++ caa/urls.py
@@ -1,17 +1,23 @@
 from django.conf.urls import patterns, include, url
+from django.contrib import admin
 
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from caa import officers, schedules, marathons
+
+admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'caa.views.home', name='home'),
-    # url(r'^caa/', include('caa.foo.urls')),
+    # Current schedule
+    url(r'^schedules/current/$', schedules.views.CurrentSchedule.as_view(), name='caa_current_schedule'),
+    
+    # Previous schedule
+    url(r'^schedules/(?P<year>\d{4})/(?P<semester>Fall|Spring)/$', schedules.views.ScheduleDetail.as_view(), name='caa_schedule'),
 
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    # Officers listing
+    url(r'^officers/$', officers.views.OfficersList.as_view(), name='caa_officers'),
 
-    # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+    # Previous marathon
+    url(r'^marathons/(?P<year>\d{4})/(?P<semester>Fall|Spring)/$', marathons.views.MarathonDetail.as_view(), name='caa_marathon'),
+
+    # Enable admin
+    url(r'^admin/', include(admin.site.urls)),
 )
