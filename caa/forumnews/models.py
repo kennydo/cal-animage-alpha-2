6bfc1b9b4d5328971cce6501b1ce73818f45695e
Kenny Do <kennydo@berkeley.edu>
2012-08-21 00:28:18 -0700

    posts are only counted as comments if they're approved

--- caa/forumnews/models.py
+++ caa/forumnews/models.py
@@ -41,6 +41,7 @@ class Post(models.Model):
     topic = models.ForeignKey(Topic)
     forum = models.ForeignKey(Forum)
     poster = models.ForeignKey(User)
+    post_approved = models.BooleanField(required=False)
     post_time = models.IntegerField()
     bbcode_uid = models.CharField(max_length=8)
     post_text = models.TextField()
@@ -49,7 +50,7 @@ class Post(models.Model):
         return datetime.datetime.fromtimestamp(self.post_time)
 
     def num_comments(self):
-        return self.first_post_of_topic.post_set.count() - 1
+        return self.first_post_of_topic.post_set..filter(post_approved=True).count() - 1
 
     def bbcode_text(self):
         text = self.post_text
